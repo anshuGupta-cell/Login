32b981599724a1764793be812a75c3105190e573
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2026-03-19 21:04:45 +0530

    fixing

--- Backend/login.js
+++ Backend/login.js
@@ -20,7 +20,7 @@ export const insertDetails = async (req, res) => {
     const platform = req.headers['sec-ch-ua-platform'];
     const userAgent = req.headers['user-agent'];
     console.log(typeof platform, typeof userAgent);
-    
+    // console.log(data);
 
     const ip =
         req.headers["cf-connecting-ip"] ||
@@ -29,7 +29,7 @@ export const insertDetails = async (req, res) => {
         req.socket.remoteAddress ||
         req.ip
 
-    const result = await pool.query(`INSERT INTO users (ip, ipv4, date, battery_percentage, platform, user_agent) VALUES ($1, $2, $3, $4, $5)`, [ip, data.ipv4, data.date, data.battery, platform, userAgent]);
+    const result = await pool.query(`INSERT INTO users (ip, ipv4, date, battery_percentage, platform, user_agent) VALUES ($1, $2, $3, $4, $5, $6)`, [ip, data.ipv4, data.date, data.battery, platform, userAgent]);
 
     return res.send('inserted')
 }
