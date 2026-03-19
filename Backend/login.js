f08b54c18ed16a9fd3005037dc67b7b695c3bac4
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2026-03-19 18:11:26 +0530

    platform and useragent

--- Backend/login.js
+++ Backend/login.js
@@ -17,6 +17,11 @@ export const getDetails = async (req, res) => {
 // insert data in database
 export const insertDetails = async (req, res) => {
     const data = req.body
+    const platform = req.headers['sec-ch-ua-platform'];
+    const userAgent = req.headers['user-agent'];
+    console.log(typeof platform, typeof userAgent);
+    
+
     const ip =
         req.headers["cf-connecting-ip"] ||
         req.headers["x-forwarded-for"] ||
@@ -24,7 +29,7 @@ export const insertDetails = async (req, res) => {
         req.socket.remoteAddress ||
         req.ip
 
-    const result = await pool.query(`INSERT INTO users (ip, date, battery_percentage) VALUES ($1, $2, $3)`, [ip, data.date, data.battery]);
+    const result = await pool.query(`INSERT INTO users (ip, date, battery_percentage, platform, user_agent) VALUES ($1, $2, $3, $4, $5)`, [ip, data.date, data.battery, platform, userAgent]);
 
     return res.send('inserted')
 }
