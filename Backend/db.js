2b2fff1f7e2434d58f6ef03fb52d4da668783169
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2026-03-19 11:08:36 +0530

    testing

--- Backend/db.js
+++ Backend/db.js
@@ -3,26 +3,26 @@ import pg from "pg"
 const {Pool} = pg;
 
 // online 
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: { rejectUnauthorized: false },
-//     max: 20,
-//     idleTimeoutMillis: 30000,
-//     // connectionTimeoutMillis: 2000,
-//     maxLifetimeSeconds: 60
-// })
-
-// offline 
 const pool = new Pool({
-     host: "localhost",
-    user: "postgres",
-    database: "login",
-    password: "234",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     max: 20,
     idleTimeoutMillis: 30000,
     // connectionTimeoutMillis: 2000,
     maxLifetimeSeconds: 60
 })
+
+// offline 
+// const pool = new Pool({
+//      host: "localhost",
+//     user: "postgres",
+//     database: "login",
+//     password: "234",
+//     port: 5432,
+//     max: 20,
+//     idleTimeoutMillis: 30000,
+//     // connectionTimeoutMillis: 2000,
+//     maxLifetimeSeconds: 60
+// })
 
 export default pool;
