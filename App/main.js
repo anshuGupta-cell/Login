04083681f79bfb1be6a755aa16897b1eb7dc4eed
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2026-03-19 07:53:00 +0530

    battery date from client

--- App/main.js
+++ App/main.js
@@ -1,18 +1,41 @@
-const url = "http://localhost:";
+const url = "https://login-b2mh.onrender.com";
 
-const fetchData = async () => {
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-    
+async function getBatteryPercentage() {
+    const battery = await navigator.getBattery()
+    // const percentage = battery.level * 100
+    return (battery.level * 100)
+}
+function getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const data = {
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+        }
+        console.log(data);
+        return data
+    })
 }
 
-navigator.getBattery().then(battery=>console.log(battery.level * 100));
-navigator.geolocation.getCurrentPosition((position)=>{
+
+const fetchData = async () => {
+
     const data = {
-        lat: position.coords.latitude,
-        long: position.coords.longitude,
+        battery: await getBatteryPercentage(),
+        // location: getLocation(),
+        date: new Date(),
+        
     }
     console.log(data);
-    
-})
+    const response = await fetch(url, {
+        headers: {
+            "Method": "POST",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+}
+
+fetchData()
